bb2c1d33f06d47b6e8678c63ad4ead105496375d
Splice <jpf0005@gmail.com>
2018-11-01 23:07:20 -0400

    fixing smooth scroll in IE

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,13 +1,11 @@
-$(document).ready(function(){
-    if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
-        $('body').on("mousewheel", function () {
-            // remove default behavior
-            event.preventDefault(); 
+if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+    $('body').on("mousewheel", function () {
+        // remove default behavior
+        event.preventDefault(); 
 
-            //scroll without smoothing
-            var wheelDelta = event.wheelDelta;
-            var currentScrollPosition = window.pageYOffset;
-            window.scrollTo(0, currentScrollPosition - wheelDelta);
-        });
-    }
-});
+        //scroll without smoothing
+        var wheelDelta = event.wheelDelta;
+        var currentScrollPosition = window.pageYOffset;
+        window.scrollTo(0, currentScrollPosition - wheelDelta);
+    });
+}
