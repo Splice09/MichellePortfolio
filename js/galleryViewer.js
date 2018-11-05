7ffa2ca52649449075c91331f414f295100e6612
Splice <jpf0005@gmail.com>
2018-11-05 18:34:23 -0500

    setting up galleries

--- js/galleryViewer.js
+++ js/galleryViewer.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
     //OPEN GALLERY OF THUMBNAILS BASED ON ICON CLICK
-
+    $('.portfolioBox').click(function(){
+        var galleryBoxId = $(this).data('value');
+        $('#gb' + galleryBoxId).animate({
+                    opacity: 1
+            }, 'fast', function(){});
+    });
 });
