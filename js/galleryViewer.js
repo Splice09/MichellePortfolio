162cebe008b95e9706486fef2a98b07342b32c4c
Splice <jpf0005@gmail.com>
2018-11-05 18:44:49 -0500

    fixing on click

--- js/galleryViewer.js
+++ js/galleryViewer.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     //OPEN GALLERY OF THUMBNAILS BASED ON ICON CLICK
     $('.portfolioBox').click(function(){
         var galleryBoxId = $(this).data('value');
-        $('#gb' + galleryBoxId).animate({
-                    opacity: 1
-            }, 'fast', function(){});
+        $('#gb' + galleryBoxId).css({opacity: 0, display: 'flex'}).animate({
+            opacity: 1
+        }, 'fast'); 
     });
 });
