e0deec5b4cbfc32c057c8856b8d6f80f441e3db7
Splice <jpf0005@gmail.com>
2018-12-12 21:20:21 -0500

    setting up the first photo album

--- js/galleryViewer.js
+++ js/galleryViewer.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     //OPEN GALLERY OF THUMBNAILS BASED ON ICON CLICK
     $('.portfolioBox').click(function(){
         var galleryBoxId = $(this).data('value');
-        $('#gb' + galleryBoxId).css({opacity: 0, display: 'flex'}).animate({
+        $('#gb' + galleryBoxId).css({opacity: 0, display: 'block'}).animate({
             opacity: 1
         }, 'fast'); 
     });
